6bc6014945172648c7fddd40d81f98f8d59deb72
jitender singh <jeetusinghkanyal@gmail.com>
2023-07-11 11:48:07 +0530

    added HOC function

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../style";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-import { SectionWrapper } from "../hoc";
+import SectionWrapper from "../hoc/SectionWrapper";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -68,4 +68,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,10 +13,14 @@ const SectionWrapper = (Component, idName) => {
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
         <Component />
       </motion.section>
     );
   }
+  return HOC;
 };
 
 export default SectionWrapper;
